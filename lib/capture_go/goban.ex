2981eca5c4b886e830b6313ee0a02db59d4a5b70
Dan Knutson <dan.knutson@gmail.com>
2019-03-28 15:38:26 -0500

    get rid of util module

--- lib/capture_go/goban.ex
+++ lib/capture_go/goban.ex
@@ -1,6 +1,4 @@
 defmodule CaptureGo.Goban do
-  import CaptureGo.Goban.Util
-
   alias CaptureGo.Goban
 
   defstruct board: Map.new(),
@@ -177,4 +175,16 @@ defmodule CaptureGo.Goban do
     [up(point), down(point), left(point), right(point)]
     |> Enum.filter(fn point -> !off_the_board?(goban, point) end)
   end
+
+  def opposite_color(:black), do: :white
+  def opposite_color(:white), do: :black
+
+  def is_color?(:black), do: true
+  def is_color?(:white), do: true
+  def is_color?(_), do: false
+
+  def up({x, y}), do: {x, y - 1}
+  def down({x, y}), do: {x, y + 1}
+  def left({x, y}), do: {x - 1, y}
+  def right({x, y}), do: {x + 1, y}
 end
